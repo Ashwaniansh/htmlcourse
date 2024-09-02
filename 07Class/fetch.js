42f97cf6c67782b58f8230177f89b544ed6791c1
Ashwaniansh <ashwaniansh426@gmail.com>
2024-09-02 05:30:29 +0530

    Hello

---
+++ 07Class/fetch.js
@@ -0,0 +1,32 @@
+// function hello(){
+//   try{
+//     console.log('Hello')
+//   }catch(error){
+//     console.log(error.message)
+//   }
+// }
+// hello()
+
+// async function hello(){
+//   try{
+//     const response = await fetch("https://api.ipify.org/?format=json")
+//     const jsonResponse = await response.json()
+//     console.log(jsonResponse)
+//   }catch(error){
+//     console.log(error.message)
+//   }
+// }
+
+// hello()
+
+// const pincode = async () => {
+//     try{
+//        const response = await fetch("https://api.ipify.org/?format=json")
+//        const jsonResponse = await response.json()
+//        console.log(jsonResponse)
+//     } catch (error) {
+//     console.log(error.message)
+//     }
+// }
+
+// pincode()
